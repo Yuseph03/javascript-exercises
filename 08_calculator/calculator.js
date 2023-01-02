6bb7c71deca5d12427c603e216e6a9a79d9470d5
Yuseph03 <vernonnashville@gmail.com>
2023-01-02 13:06:35 +0330

    done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	let c = (a+b);
+  return c;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let x = (a - b);
+  return x;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let k = 0;
+  for(item of a){
+    k = (k + item)
+  }
+  return k;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let k = 1;
+  for(item of a){
+    k = (k * item) 
+  }
+  return k;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let x = Math.pow(a ,b);
+  return x;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    if (num === 0 || num === 1)
+      return 1;
+    for (var i = num - 1; i >= 1; i--) {
+      num *= i;
+    }
+    return num;
 };
 
 // Do not edit below this line
